c814b186437c83812833e536ee007861d7ee711c
Aperence <anthony.doeraene@hotmail.com>
2023-08-09 17:06:12 +0200

    added readme

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,5 +1,6 @@
 use std::process::{Command, Output};
 
+
 /**
  * Run a cmd and return the result of this cmd
  */
@@ -9,4 +10,20 @@ pub fn run_task(cmd : &str) -> Output{
                 .arg(cmd)
                 .output()
                 .expect(format!("Failed to run {}", cmd).as_str())
+}
+
+#[cfg(test)]
+mod tests{
+
+    use crate::tasks::run_task;
+    use std::str;
+
+    #[test]
+    fn test_run_task(){
+        let out = run_task("cat src/tasks.rs | wc -l");
+
+        assert_eq!(out.status.code().unwrap(), 0);
+        assert_eq!(str::from_utf8(&out.stdout).unwrap().trim(), "28");
+        assert_eq!(str::from_utf8(&out.stderr).unwrap().trim(), "");
+    }
 }
